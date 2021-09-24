2ed3078aff9ea5e8728e6b656d89042b8a9fb103
tyhess <tyhess@overstock.com>
2021-09-24 08:03:42 -0600

    Class breakout breaking classes out for better clarity and ease of handling conversions, syntax, and context checking.

--- squawker.py
+++ squawker.py
@@ -1,5 +1,6 @@
 from ravenrpc import Ravencoin
 from credentials import USER, PASSWORD
+from account import Account
 import json
 import time
 import ipfshttpclient
@@ -84,57 +85,6 @@ def recursive_print(dictionary, spacing=0):
                 print()
 
 
-class Profile:
-    def __init__(self, config_file):
-        with open(config_file, 'r') as config_file_handle:
-            self.config = json.load(config_file_handle)
-
-    def __getattr__(self, name):
-        if name.startswith('__') and name.endswith('__'):
-            # Python internal stuff
-            raise AttributeError
-
-        if name in self.config:
-            return self.config[name]
-        elif name in self.__dict__:
-            return self.__dict__[name]
-        else:
-            raise AttributeError
-
-    def send_kaw(self, message):
-        hash = ipfs.add_json(self.compile_message(message))
-        ipfs.pin.add(hash)
-        # transfer asset_name, qty, to_address, message, expire_time, change_address, asset_change_address
-        return rvn.transfer(ASSETNAME, 1, self.address, hash, 0, self.address, self.address)
-
-    def compile_message(self, text, multimedia=None):
-        message = dict()
-        message["sender"] = self.address
-        message["profile"] = {"ipfs_hash": self.profile_ipfs_hash, "timestamp": self.profile_timestamp}
-        message["timestamp"] = time.time()
-        message["message"] = text
-        if multimedia:
-            message["multimedia"] = [media["ipfs_hash"] for media in multimedia]
-        return message
-
-    def update_profile(self):
-        message = dict()
-        self.config["profile_timestamp"] = time.time()
-        for att in self.config:
-            if att == "profile_timestamp":
-                message["timestamp"] = self.config[att]
-            elif att == "profile_hash":
-                pass
-            else:
-                message[att] = self.config[att]
-
-        hash = ipfs.add_json(message)
-        ipfs.pin.add(hash)
-        self.config["profile_hash"] = hash
-        with open("config.json", 'w') as cf:
-            json.dump(self.config, cf)
-
-        return rvn.transfer(ASSETNAME, 0.5, self.address, hash, 0, self.address, self.address)
 
 
 """
@@ -169,7 +119,7 @@ setup atomic swaps for marketplace sales.
 """
 
 if __name__ == "__main__":
-    usr = Profile("config.json")
+    usr = Account("config.json", ASSETNAME, rvn, ipfs)
     while True:
         intent = input("Kaw (1) | Read (2) | Update Profile (3) | Exit (4)")
         if str(intent).strip() == "1":
